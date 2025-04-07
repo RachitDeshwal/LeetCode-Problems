af2daa8dc4983e65a829e002b9433fdac4aabd3a
Rachit Deshwal <rachitdeshwal0001@gmail.com>
2025-04-07 23:55:21 +0530

    Time complexity-O(N) Space complexity-O(1)

--- 141🟢Linked List cycle/Solution.java
+++ 141🟢Linked List cycle/Solution.java
@@ -12,4 +12,44 @@ public class Solution {
       return false;
       
   }
+}class Solution {
+    public Node copyRandomList(Node head) {
+        if(head==null){
+            return null;
+        }
+        Node curr=head;
+        // step 1 create seprate copy of same values that assinged b/w list nodes
+        while(curr!=null){
+            Node newNode= new Node(curr.val);
+            newNode.next=curr.next;
+            curr.next=newNode;
+            curr=newNode.next;
+
+        }
+        //step 2 copy random pointers
+        curr=head;
+        while(curr!=null){
+            if(curr.random!=null){
+
+            
+            curr.next.random=curr.random.next;
+            
+        }
+        curr=curr.next.next;
+        }
+        //step 3 seprate both lists
+        curr=head;
+        Node newHead=head.next;
+        Node newcurr=curr.next;
+        while(curr!=null){
+            curr.next=newcurr.next;
+            curr=curr.next;
+            if(curr!=null){
+                newcurr.next=curr.next;
+                newcurr=newcurr.next;
+            }
+        }
+        return newHead;
+        
+    }
 }
