ecc07e56874a6f368a7a82e53a4aab62039a6988
RachitDeshwal <rachitdeshwal0001@gmail.com>
2025-08-03 22:38:16 +0530

    Time complexity-O(N) Space complexity-O(1)

---
+++ 121🟢Best time to buy and sell stocks/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        int buy_price=prices[0];
+        for(int i=0;i<prices.length;i++){
+            if(buy_price>prices[i]){
+                buy_price=prices[i];
+            }
+            profit=Math.max(profit,prices[i]-buy_price);
+        }
+        return profit;
+
+        
+    }
+}
