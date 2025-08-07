eb02c01fd273449ea3bf32aa6e82d5d64299980e
RachitDeshwal <rachitdeshwal0001@gmail.com>
2025-08-07 22:33:45 +0530

    Time complexity-O(N) Space complexity-O(1)

---
+++ 328🟠Odd even Linked List/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null){
+            return null;
+        }
+       ListNode odd=head;
+       ListNode even=head.next;
+       ListNode evenHead=even;
+       while(even!=null&&even.next!=null){
+        odd.next=odd.next.next;
+        odd=odd.next;
+        even.next=even.next.next;
+        even=even.next;
+
+       }
+       odd.next=evenHead;
+       return head;
+    }
+}
